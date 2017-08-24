6fb89935f178d330c62dd527bde862714701c62c
Kevin Thibodeaux <kthib16@gmail.com>
2017-08-24 13:01:05 -0400

    Checkpoint 33 completed

--- scripts/album.js
+++ scripts/album.js
@@ -1,6 +1,23 @@
 var setSong = function(songNumber){
-    var currentlyPlayingSongNumber = parseInt(songNumber);
-    var currentSongFromAlbum = currentAlbum.songs[songNumber - 1];
+    if(currentSoundFile) {
+        currentSoundFile.stop();
+    }
+
+    currentlyPlayingSongNumber = parseInt(songNumber);
+    currentSongFromAlbum = currentAlbum.songs[songNumber - 1];
+
+    currentSoundFile = new buzz.sound(currentSongFromAlbum.audioUrl, {
+        formats: [ 'mp3' ],
+        preload: true
+    });
+
+    setVolume(currentVolume);
+};
+
+var setVolume = function(volume) {
+    if(currentSoundFile) {
+        currentSoundFile.setVolume(volume);
+    }
 };
 
 var getSongNumberCell = function(number) {
@@ -34,12 +51,20 @@ var createSongRow = function(songNumber, songName, songLength) {
     		$(this).html(pauseButtonTemplate);
     		setSong(songNumber);
         updatePlayerBarSong();
+        currentSoundFile.play();
 
     	} else if (currentlyPlayingSongNumber === songNumber) {
-    		$(this).html(playButtonTemplate);
-        $('.main-controls .play-pause').html(playerBarPlayButton);
-    		currentlyPlayingSongNumber = null;
-        currentSongFromAlbum = null;
+        if(currentSoundFile.isPaused()){
+            $(this).html(pauseButtonTemplate);
+            $('.main-controls .play-pause').html(playerBarPauseButton);
+            currentSoundFile.play();
+        }
+
+        else {
+            $(this).html(playButtonTemplate);
+            $('.main-controls .play-pause').html(playerBarPlayButton);
+            currentSoundFile.pause();
+        }
     	}
   };
 
@@ -107,7 +132,7 @@ var setCurrentAlbum = function(album) {
    var lastSongNumber = currentlyPlayingSongNumber;
 
    setSong(currentSongIndex + 1);
-
+   currentSoundFile.play();
    updatePlayerBarSong();
 
    var $nextSongNumberCell = getSongNumberCell(currentlyPlayingSongNumber);
@@ -128,7 +153,7 @@ var setCurrentAlbum = function(album) {
    var lastSongNumber = currentlyPlayingSongNumber;
 
    setSong(currentSongIndex + 1);
-
+   currentSoundFile.play();
    updatePlayerBarSong();
 
    var $previousSongNumberCell = getSongNumberCell(currentlyPlayingSongNumber);
@@ -153,6 +178,8 @@ var playerBarPauseButton = '<span class="ion-pause"></span>';
 var currentAlbum = null;
 var currentlyPlayingSongNumber = null;
 var currentSongFromAlbum = null;
+var currentSoundFile = null;
+var currentVolume = 80;
 
 var $previousButton = $('.main-controls .previous');
 var $nextButton = $('.main-controls .next');
